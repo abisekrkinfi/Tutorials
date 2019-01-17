18d22db9ea825c666e7a4650bed85045366d7ea2
Abisek R K <abisek.rajakumar@ia.ooo>
2019-01-17 18:16:31 +0530

    Java script practice

---
+++ JS/secondJs.js
@@ -0,0 +1,17 @@
+/*
+function fact(var f){
+
+  if (f==1){
+    return f;
+  }
+
+  else{
+    return f* fact(f-1);
+  }
+}
+*/
+function myFunction() {
+  var x=5;
+  alert("check");
+  alert("Factorial of "+x+" is "+ fact(x));
+}
